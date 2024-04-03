2ab3aff7c7ccad4ef4bcf197e3dd271a483837bc
Alex Fix <dasdias@yandex.ru>
2024-04-03 17:58:22 +0300

    created card

--- scripts/card.js
+++ scripts/card.js
@@ -38,8 +38,36 @@ export const form = () => {
     for: 'input__holder'}, 'Card Holder');
   const input = el('input', {className: 'input input__holder',
     type: 'text', id: 'input__holder'});
+
+  const formInputWrapNumber = el('div',
+      {className: 'form__input-wrap form__input-wrap_number'});
+  const labelNumber = el('label', {className: 'form__label form__number-label',
+    for: 'input__number'}, 'Card Number');
+  const inputNumber = el('input', {className: 'input input__holder',
+    type: 'text', id: 'input__number'});
+
+  const formInputWrapDate = el('div',
+      {className: 'form__input-wrap form__input-wrap_date'});
+  const labelDate = el('label', {className: 'form__label form__date-label',
+    for: 'cardNumber'}, 'Card Expiry');
+  const inputDate = el('input', {className: 'input input__date',
+    type: 'text', id: 'cardNumber'});
+
+  const formInputWrapCvv = el('div',
+      {className: 'form__input-wrap form__input-wrap_cvv'});
+  const labelCvv = el('label', {className: 'form__label form__cvv-label',
+    for: 'cardNumber'}, 'CVV');
+  const inputCvv = el('input', {className: 'input input__cvv',
+    type: 'text', id: 'cardNumber'});
+
+  const button = el('button', {className: 'form__button'}, 'CHECK OUT');
+
   setChildren(formInputWrap, [label, input]);
-  setChildren(formElem, formInputWrap);
+  setChildren(formInputWrapCvv, [labelCvv, inputCvv]);
+  setChildren(formInputWrapDate, [labelDate, inputDate]);
+  setChildren(formInputWrapNumber, [labelNumber, inputNumber]);
+  setChildren(formElem, [formInputWrap, formInputWrapNumber,
+    formInputWrapDate, formInputWrapCvv, button]);
   return formElem;
 };
 
